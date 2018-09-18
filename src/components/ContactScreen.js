e5852e7415addfb83dacdcd5f5716ca001023035
Gray Gunter <gray.gunter@gmail.com>
2018-09-18 16:25:53 -0400

    Created the contact form with Cognito

--- src/components/ContactScreen.js
+++ src/components/ContactScreen.js
@@ -2,80 +2,19 @@ import React, { Component } from 'react';
 
 class ContactScreen extends Component {
 
-  constructor() {
-
-    super();
-
-    this.handleSubmit = this.handleSubmit.bind(this);
-
-    this.state = {
-      feedback: "",
-      formEmailSent: false,
-      formSubmitted: false,
-      senderEmail: "",
-    }
-
-  }
-
-  handleSubmit(event) {
-    event.preventDefault()
-
-    const {
-      REACT_APP_EMAILJS_RECEIVER: receiverEmail,
-      REACT_APP_EMAILJS_TEMPLATEID: template
-    } = this.props.env
-
-    this.sendFeedback(
-      template,
-      this.state.senderEmail,
-      receiverEmail,
-      this.state.feedback)
-
-    this.setState({
-      formSubmitted: true
-    })
-  }
-
-  sendFeedback(templateId, senderEmail, receiverEmail, feedback) {
-    window.emailjs.send(
-      'mailgun',
-      templateId,
-      {
-        senderEmail,
-        receiverEmail,
-        feedback
-      })
-      .then(res => {
-        this.setState({ formEmailSent: true })
-      })
-      // Handle errors here however you like, or use a React error boundary
-      .catch(err => console.error('Failed to send feedback. Error: ', err))
-  }
-
   render() {
     return (
 
-      <div className="contactScreen">
-
-        <form className="contact" onSubmit={this.handleSubmit}>
-          <h1>Contact Graybear</h1>
-
-          <input type="text"
-              defaultValue={"Your email"}
-              ref={input => this.inputField = input}
-              required
-              onFocus={() => this.inputField.value = ""}/>
-
-          <textarea
-                    defaultValue={"Blah blah blah"}
-                    ref={input2 => this.inputField2 = input2}                  
-                    required
-                    onFocus={() => this.inputField2.value = ""}/>  
-
-          <input type="submit" value="Send" />
-
-        </form>
-
+      <div className="cognito">
+        <iframe 
+                frameBorder="0" 
+                height="450" 
+                scrolling="yes" 
+                seamless="seamless" 
+                src="https://services.cognitoforms.com/f/213mxnhu9EiHnPTd9v0xfA?id=1" 
+                style={{minWidth: 100 + '%', width: 100 + '%'}} 
+                width="100%"></iframe>
+        <script src="https://services.cognitoforms.com/scripts/embed.js"></script>
       </div>
 
     );
@@ -83,3 +22,5 @@ class ContactScreen extends Component {
 }
 
 export default ContactScreen;
+
+/* The `style` prop expects a mapping from style properties to values, not a string. For example, style={{marginRight: spacing + 'em'}} when using JSX. */
